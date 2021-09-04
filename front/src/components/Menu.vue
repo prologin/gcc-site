e0086a2f7bdf7a97d4c80173d4d3ca9b65563fa3
James <me@j4m3s.eu>
2021-09-04 19:59:06 +0200

    front: add base css

--- front/src/components/Menu.vue
+++ front/src/components/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="menu">
     <img class="menu-logo" src="../assets/GCC_3.svg" />
-    <div v-if="isConnected">
-       <a class="secondary-button" id="inscription" > S'inscrire </a> <!-- TODO: add link to ProloginAuth -->
-       <a class="primary-button" id="connect" > Se connecter </a>
+    <div class="nav-buttons" v-if="isConnected">
+       <a class="button secondary-button" id="inscription" > S'inscrire </a> <!-- TODO: add link to ProloginAuth -->
+       <a class="button primary-button" id="connect" > Se connecter </a>
     </div>
     <div v-else>
       Bonjour {{ name }} {{ surname }} !
@@ -27,13 +27,24 @@ export default defineComponent({
 
 <style scoped>
 .menu-logo {
-  width: 5%;
+  width: 5em;
   padding-left: 5%;
 }
 
+.menu {
+  padding-top: 0.7em;
+  flex-wrap: wrap;
+  display: flex; /* flexbox */
+  justify-content: space-between; /* space between direct children */
+  align-items: center;
+}
+
 #connect {
-  padding-right: 2%;
-  padding-left: 2%;
-  position: fixed;
+}
+
+.nav-buttons {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 }
 </style>
