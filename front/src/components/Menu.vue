b1187ba5c7306ad5355691e204da6cf0686ea91a
James <me@j4m3s.eu>
2021-09-04 19:04:04 +0200

    feat(front): add beginning of menu
    Signed-off-by: James <me@j4m3s.eu>

---
+++ front/src/components/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="menu">
+    <img class="menu-logo" src="../assets/GCC_3.svg" />
+    <div v-if="isConnected">
+       <a class="secondary-button" id="inscription" > S'inscrire </a> <!-- TODO: add link to ProloginAuth -->
+       <a class="primary-button" id="connect" > Se connecter </a>
+    </div>
+    <div v-else>
+      Bonjour {{ name }} {{ surname }} !
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Menu',
+  props: {
+    isConnected: Boolean,
+    name: String,
+    surname: String
+  }
+})
+</script>
+
+<style scoped>
+.menu-logo {
+  width: 5%;
+  padding-left: 5%;
+}
+
+#connect {
+  padding-right: 2%;
+  padding-left: 2%;
+  position: fixed;
+}
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Menu isConnected="false" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import Menu from '@/components/Menu.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    Menu
   }
 })
 </script>
