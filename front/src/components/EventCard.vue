3be1fe636e95c67f74265f25aff3f37e291d3d2e
Yona <aliona.dangla@prologin.org>
2021-09-08 23:10:27 +0200

    front: Add bullet color function

--- front/src/components/EventCard.vue
+++ front/src/components/EventCard.vue
@@ -1,13 +1,13 @@
 <template>
     <b-row class="event-card mb-4 p-3">
         <b-col class="my-auto" md="auto">
-            <b-icon icon="circle-fill" aria-hidden="true"></b-icon>
+            <b-icon v-bind:class="colorClass" icon="circle-fill" aria-hidden="true"/>
         </b-col>
         <!-- Event infos verticaly centered -->
         <b-col cols="auto" class="my-auto">
             <h1>{{ title }}</h1>
             <h2>{{ date }}</h2>
-            <h3>{{ adress }}</h3>
+            <h3>{{ address }}</h3>
         </b-col>
         <b-button-group vertical class="my-auto ml-auto m-5" cols="auto">
             <b-button class="mx-4 primary-button">S'inscrire à ce stage</b-button>
@@ -21,7 +21,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'EventCard',
-  props: ['title', 'date', 'adress']
+  props: ['title', 'date', 'address', 'index'],
+  computed: {
+    colorClass: function () {
+      var colors = ['gcc-pink', 'gcc-green', 'gcc-blue']
+      return colors[this.index % colors.length]
+    }
+  }
 })
 </script>
 
--- front/src/components/EventList.vue
+++ front/src/components/EventList.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="background-style pb-4">
         <section class="w-75 mx-auto pt-5">
-            <EventCard v-for="post in posts"
-                       v-bind:key="post.id"
-                       v-bind:title="post.title"
-                       v-bind:date="post.date"
-                       v-bind:adress="post.adress"/>
+            <EventCard v-for="(event, index) in events"
+                       v-bind:index="index"
+                       v-bind:key="event.id"
+                       v-bind:title="event.title"
+                       v-bind:date="event.date"
+                       v-bind:address="event.address"/>
         </section>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import EventCard from './EventCard.vue'
+import EventCard from '@/components/EventCard.vue'
 
 export default Vue.extend({
   name: 'EventList',
@@ -22,8 +23,8 @@ export default Vue.extend({
   },
   data () {
     return {
-      posts: [
-        { id: 1, title: 'Stage long à EPITA Paris', date: '12 janvier au 15 décembre 1245', adress: 'EPITA Paris, 14-16 rue Voltaire, 94270 Le Kremlin-Bicêtre' },
+      events: [
+        { id: 1, title: 'Stage long à EPITA Paris', date: '12 janvier au 15 décembre 1245', address: 'EPITA Paris, 14-16 rue Voltaire, 94270 Le Kremlin-Bicêtre' },
         { id: 2, title: 'Stage court à EPITA Paris' },
         { id: 3, title: 'Stage long à EPITA Lyon' }
       ]
@@ -45,7 +46,7 @@ export default Vue.extend({
     border-radius: 12px;
 }
 
-.btn-group-vertical > button{
+.btn-group-vertical > button {
     margin-bottom:5px;
     border-radius:12px !important;
 }
