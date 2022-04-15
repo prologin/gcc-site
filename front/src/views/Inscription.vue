6bb7fdcbae6f19341cfdbeb9fc9310b6d1586fa0
James <me@j4m3s.eu>
2022-04-15 20:23:04 +0200

    feat(front): add Form decoration

--- front/src/views/Inscription.vue
+++ front/src/views/Inscription.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    <ContactInformationComponent />
-    <ParticipantInformationComponent />
-    <QuestionsComponent />
-  </div>
+  <b-row class="code-background">
+    <b-col class="p-0">
+      <!-- TODO: add optimized images -->
+      <img class="w-100" :src="require('@/assets/gcc-affiche-2022.jpg')" alt="Affiche GCC decoration" />
+    </b-col>
+    <b-col md="8" class="white-background">
+      <ContactInformationComponent />
+      <ParticipantInformationComponent />
+      <QuestionsComponent />
+
+      <b-row align-h="end">
+        <b-col cols="4">
+          <b-button class="primary-button" block>Continuer</b-button>
+        </b-col>
+      </b-row>
+
+    </b-col>
+  </b-row>
 </template>
 
 <script lang="ts">
@@ -21,3 +34,19 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+.white-background {
+  background-color: white;
+}
+
+.code-background {
+  height: 100%;
+  background-attachment: fixed;
+  background-color: var(--main-light-color);
+  background-image: url('~@/assets/faq_bg.svg');
+  background-repeat: no-repeat;
+  background-size: 70%;
+  background-position: left bottom;
+}
+</style>
