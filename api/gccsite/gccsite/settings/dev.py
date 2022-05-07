03dc4b6d3f45ee8f7e0c22dcc604191825c12c42
Kenji Gaillac <kenji.gaillac@prologin.org>
2022-05-07 18:42:43 +0200

    api: add logging in dev config

--- api/gccsite/gccsite/settings/dev.py
+++ api/gccsite/gccsite/settings/dev.py
@@ -16,3 +16,27 @@ AWS_S3_ENDPOINT_URL = "CHANGE_ME"
 AWS_STORAGE_BUCKET_NAME = "gcc-site-dev"
 AWS_ACCESS_KEY_ID = "CHANGE_ME"
 AWS_SECRET_ACCESS_KEY = "CHANGE_ME"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "[{levelname}] {asctime} {module} - {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
