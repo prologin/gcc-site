c5f75b734f969ec99e7a4b8d53527fb6878e789c
retke <laggron42@gmail.com>
2022-05-09 20:04:26 +0200

    date: store name lists then access

--- front/src/services/date.ts
+++ front/src/services/date.ts
@@ -1,11 +1,11 @@
 export const formatDate = function (dateUtc: string) {
   const date: Date = new Date(dateUtc)
-  const day: string = [
+  const daysNames: Array<string> = [
     'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'
-  ][date.getDay()]
-  const month: string = [
+  ]
+  const monthsNames: Array<string> = [
     'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
     'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
-  ][date.getMonth()]
-  return day + ' ' + date.getDate() + ' ' + month
+  ]
+  return daysNames[date.getDay()] + ' ' + date.getDate() + ' ' + monthsNames[date.getMonth()]
 }
