2382c1f9b92a9dfefabc9395c61c9310cd8226f3
Kenji Gaillac <kenji.gaillac@prologin.org>
2022-05-10 14:16:43 +0200

    api: applications: add filters

--- api/gccsite/events/views/signup.py
+++ api/gccsite/events/views/signup.py
@@ -1,3 +1,4 @@
+from django_filters import rest_framework as filters
 from rest_framework import permissions, viewsets
 
 from .. import models, serializers
@@ -9,10 +10,22 @@ class FormViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.FormSerializer
 
 
+class ApplicationFilter(filters.FilterSet):
+    user_id = filters.NumberFilter(field_name="user__id")
+    event_id = filters.NumberFilter(field_name="event__id")
+
+    class Meta:
+        model = models.Application
+        fields = ["user_id", "event_id", "status"]
+
+
 class ApplicationViewset(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.ApplicationSerializer
     http_method_names = ["get", "post", "delete"]
+
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ApplicationFilter
 
     def get_queryset(self):
         if self.request.user.is_staff:
