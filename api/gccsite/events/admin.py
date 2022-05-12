c4c4a6f0ac155975e8beb12c422da58aed6e3f87
Kenji Gaillac <kenji.gaillac@prologin.org>
2022-05-12 11:48:23 +0200

    api: applications: edit field order in admin

--- api/gccsite/events/admin.py
+++ api/gccsite/events/admin.py
@@ -87,9 +87,9 @@ class FormAnswerInline(admin.TabularInline):
 @admin.register(models.Application)
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = (
+        "user",
         "first_name",
         "last_name",
-        "user",
         "status",
         "event",
     )
