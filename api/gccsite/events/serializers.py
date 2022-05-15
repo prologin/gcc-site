2871614f4f6dfdfde4ee4f60ffcf9c162daccb2d
Kenji Gaillac <kenji.gaillac@prologin.org>
2022-05-15 18:17:03 +0200

    api: application: done create endpoint

--- api/gccsite/events/serializers.py
+++ api/gccsite/events/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 from . import models
@@ -120,60 +121,127 @@ class FormSerializer(serializers.ModelSerializer):
         )
 
 
+class FormAnswerValidator:
+    def __call__(self, value):
+        question, answers = value["question"], value["answer"].splitlines()
+
+        if question.type in (
+            models.QuestionType.CHOICE.value,
+            models.QuestionType.MULTIPLE_CHOICES.value,
+        ):
+            possible_answers = question.answers.splitlines()
+            for answer in answers:
+                if answer not in possible_answers:
+                    raise serializers.ValidationError(
+                        {
+                            "answer": _(
+                                f'"{answer}" n\'est pas un choix possible'
+                                " (choisir parmi :"
+                                f' {", ".join(possible_answers)})'
+                            )
+                        }
+                    )
+
+
 class FormAnswerSerializer(serializers.ModelSerializer):
-    question = QuestionSerializer()
-
-    class Meta:
-        model = models.FormAnswer
-
-        fields = (
-            "id",
-            "question",
-            "answer",
-        )
-
-        read_only_fields = ("id",)
-
-
-class FormAnswerPushSerializer(serializers.ModelSerializer):
     question = serializers.PrimaryKeyRelatedField(
         queryset=models.Question.objects.all()
     )
 
     class Meta:
         model = models.FormAnswer
+
         fields = (
             "id",
             "question",
             "answer",
         )
+
         write_only_fields = ("application_id",)
         read_only_fields = ("id",)
 
+        validators = [FormAnswerValidator()]
+
+
+class ApplicationValidator:
+    def __call__(self, value):
+        answers = value.get("form_answers", [])
+        answers_ids_set = {answer["question"].id for answer in answers}
+
+        event = models.Event.objects.get(pk=value["event"].id)
+        questions = event.get_application_questions()
+
+        questions_ids_set = set(questions.values_list("id", flat=True))
+        mandatory_questions_ids_set = set(
+            questions.filter(mandatory=True).values_list("id", flat=True)
+        )
+
+        if len(answers) != len(answers_ids_set):
+            raise serializers.ValidationError(
+                {
+                    "form_answers": _(
+                        "Vous avez répondu plusieurs fois à la même question"
+                    )
+                }
+            )
+
+        if answers_ids_set < mandatory_questions_ids_set:
+            raise serializers.ValidationError(
+                {
+                    "form_answers": _(
+                        "Certaines questions obligatoires n'ont pas de réponse"
+                    )
+                }
+            )
+
+        if answers_ids_set - questions_ids_set:
+            raise serializers.ValidationError(
+                {
+                    "form_answers": _(
+                        "Vous avez répondu à des questions non demandées"
+                    )
+                }
+            )
+
 
 class ApplicationSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(
-        allow_null=True,
-        queryset=get_user_model().objects.all(),
-    )
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    form_answers = FormAnswerSerializer(many=True, required=False)
 
-    status = serializers.ChoiceField(
-        choices=models.SelectionStatus.choices,
-        allow_null=True,
-    )
+    class Meta:
+        model = models.Application
+        fields = (
+            "id",
+            "user",
+            "event",
+            "first_name",
+            "last_name",
+            "dob",
+            "status",
+            "form_answers",
+        )
 
-    form_answers = FormAnswerPushSerializer(many=True)
+        read_only_fields = ("id", "user", "status")
+
+        validators = [ApplicationValidator()]
 
     def create(self, validated_data):
         request = self.context["request"]
-        user = request.user
-        validated_data["user"] = user
+
+        validated_data["user"] = request.user
         validated_data["status"] = models.SelectionStatus.ENROLLED.value
-        form_answers = validated_data.pop("form_answers")
+
+        try:
+            form_answers = validated_data.pop("form_answers")
+        except KeyError:
+            form_answers = []
+
         ret = super().create(validated_data)
+
         for answer in form_answers:
             answer["application_id"] = ret.id
-            FormAnswerPushSerializer().create(answer)
+            FormAnswerSerializer().create(answer)
+
         return ret
 
 
