1c872d79473e8b55aa9ce54bbf862968dd52120b
retke <laggron42@gmail.com>
2022-05-17 11:50:27 +0200

    events: remove type hinting

--- api/gccsite/events/views/events.py
+++ api/gccsite/events/views/events.py
@@ -25,7 +25,7 @@ Examples
 from django.utils import timezone
 from django_filters import rest_framework as filters
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import permissions, request, response, viewsets
+from rest_framework import permissions, response, viewsets
 from rest_framework.decorators import action
 
 from .. import models, serializers
@@ -120,12 +120,10 @@ class EventViewset(viewsets.ReadOnlyModelViewSet):
     @swagger_auto_schema(
         responses={200: serializers.EventDocumentSerializer(many=True)}
     )
-    def docs(self, request: request.Request, pk):
-        event: models.Event = self.get_object()
+    def docs(self, request, pk):
+        event = self.get_object()
         try:
-            attendee: models.Attendee = models.Attendee.objects.get(
-                owner=request.user
-            )
+            attendee = models.Attendee.objects.get(owner=request.user)
         except models.Attendee.DoesNotExist:
             docs = event.get_attendee_documents(None)
         else:
