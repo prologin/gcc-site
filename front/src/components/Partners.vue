59c04ede2fcd7dc0caf843719898ccc89feb2bae
Julien Bestard <julien.bestard@epita.fr>
2022-09-20 16:16:53 +0200

    Sponsors Display : Remove carousel because ugly asf

--- front/src/components/Partners.vue
+++ front/src/components/Partners.vue
@@ -3,7 +3,7 @@
     <b-row align-h="center" class="pt-3">
       <h1 v-if="isFeaturedSpace">Ils nous soutiennent</h1>
     </b-row>
-    <b-row v-if="isFeaturedSpace" align-h="center">
+    <b-row align-h="center">
       <b-link v-b-modal="'modal-' + partner.name" v-for="(partner, index) in partnersSortedByOrder" :key="index">
         <b-img class="partners-logo-style m-5" :src=partner.logo width="200" height="100" />
         <b-modal :id="'modal-' + partner.name" size="lg" centered ok-only hide-header ok-title="Fermer"
@@ -21,44 +21,6 @@
         </b-modal>
       </b-link>
     </b-row>
-
-    <b-row align-h="center" class="pt-3">
-      <b-carousel v-if="isFrontPage && !isFeaturedSpace" id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        background="#ababab"
-        width="200"
-        height="100"
-        img-width="200"
-        img-height="100"
-        style="width:200px;height:100px"
-        align-h="center"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd">
-
-      <!-- Text slides with image -->
-      <b-link v-b-modal="'modal-' + partner.name" v-for="(partner, index) in partnersSortedByOrder" :key="index">
-        <b-carousel-slide :img-src=partner.logo></b-carousel-slide>
-        <b-modal :id="'modal-' + partner.name" size="lg" centered ok-only hide-header ok-title="Fermer"
-          ok-variant="secondary-button">
-          <b-card :img-src=partner.logo :img-alt="`Logo de notre partenaire ${partner.name}`" img-width=250 img-left
-            class="px-3" :title="partner.name" title-tag="h1">
-
-            <b-card-text>
-              <p>{{ partner.description }}</p>
-            </b-card-text>
-            <b-card-text>
-              <a :href="partner.website_url">Voir leur site</a>
-            </b-card-text>
-          </b-card>
-        </b-modal>
-      </b-link>
-
-      </b-carousel>
-    </b-row>
-
     <b-row align-h="center" class="mb-3">
       <b-button v-if="isFrontPage && !isFeaturedSpace" :to="{ name: 'partners' }" class="secondary-button">Voir tous nos
         partenaires
@@ -80,8 +42,7 @@ export default Vue.extend({
   ],
   data () {
     return {
-      partners: [],
-      sliding: null
+      partners: []
     }
   },
   async created () {
@@ -95,14 +56,6 @@ export default Vue.extend({
   computed: {
     partnersSortedByOrder () {
       return this.partners.slice().sort((a, b) => a.order - b.order)
-    }
-  },
-  methods: {
-    onSlideStart () {
-      this.sliding = true
-    },
-    onSlideEnd () {
-      this.sliding = false
     }
   }
 })
