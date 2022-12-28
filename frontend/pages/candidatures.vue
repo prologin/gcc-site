6d6110d565f799fe16c04c4d11a371d13823bd52
Julien Bestard <julien.bestard@epita.fr>
2022-12-28 08:52:45 +0100

    change name of page

---
+++ frontend/pages/candidatures.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-body-style">
+    <b-container fluid class="p-0">
+      <!-- Header -->
+      <Header />
+
+
+
+      <!-- Footer -->
+      <Footer />
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+
+export default Vue.extend({
+  name: "Candidatures",
+  components: {
+    Header,
+    Footer,
+  },
+});
+</script>
