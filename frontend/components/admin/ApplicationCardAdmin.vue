80c2d8b4ca15544990fe4b94afccf8ad5a666bbf
Julien Bestard <julien.bestard@epita.fr>
2023-01-08 16:49:47 +0100

    change styling, trying ot get nb of particpation

--- frontend/components/admin/ApplicationCardAdmin.vue
+++ frontend/components/admin/ApplicationCardAdmin.vue
@@ -7,19 +7,21 @@
     ok-variant="secondary-button"
     size="xl"
   >
-    Infos de la candidate
+    <span :id="app.user"> {{ this.user.particaptions_count }} </span>
   </b-modal>
 </template>
 <script>
 import Vue from "vue";
 import { applicationsAPI } from "../../services/applications.api";
 import { eventsAPI } from "../../services/events.api";
+import { usersAPI } from "../../services/users.api";
+
 
 export default Vue.extend({
   name: "ApplicationCardAdmin",
   props: ["app_id", "event_id"],
   data() {
-    return { app: [], form: {} };
+    return { app: [], form: {}, user: []};
   },
   created() {
     applicationsAPI.applicationsRead(this.app_id).then((res) => {
@@ -27,6 +29,9 @@ export default Vue.extend({
     });
     eventsAPI.eventsForm(this.event_id).then((res) => {
       this.form = res;
+    });
+    usersAPI.usersMeRead(this.app.user).then((res) => {
+      this.user = res;
     })
   }
 });
--- frontend/components/admin/ApplicationListAdmin.vue
+++ frontend/components/admin/ApplicationListAdmin.vue
@@ -1,86 +1,152 @@
 <template>
   <div>
-    <table class="table table-borderless">
-      <thead>
-        <tr>
-          <th scope="col">Nom</th>
-          <th scope="col">Status</th>
-          <th scope="col">Infos</th>
-          <th scope="col" />
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(app, index) in applications" :key="index">
-          <td scope="row">{{ app.first_name }} {{ app.last_name }}</td>
-          <td>
-            {{ Status[app.status] }}
-          </td>
-          <td>
-            <b-button v-b-modal="'modal-' + app.user"> Infos Perso.</b-button>
-          </td>
-          <td>
-            <b-select :id="'select-' + app.user" right class="form-select">
-              <option :selected="app.status == -2" value="-2"> Rejeté </option>
-              <option :selected="app.status == -1" value="-1"> Abandonné </option>
-              <option :selected="app.status == 0" value="0"> Inscrit </option>
-              <option :selected="app.status == 1" value="1"> Non sélectionné </option>
-              <option :selected="app.status == 2" value="2"> Sélectionné </option>
-              <option :selected="app.status == 3" value="3"> Accepté </option>
-              <option :selected="app.status == 4" value="4"> Confirmé </option>
-            </b-select>
-          </td>
-          <!-- Modal = component a part -->
-          <AppCardAdmin :app_id="app.id" :event_id="event_id" />
-        </tr>
-      </tbody>
-    </table>
+    <b-table
+      hover
+      fixed
+      stacked="lg"
+      responsive="xl"
+      class="table-style"
+      :fields="fields"
+      :items="applications"
+      selectable
+      @row-clicked="
+        (item) =>
+          $set(item, '_showDetails', !item._showDetails && item.status != -2)
+      "
+    >
+      <template #cell(name)="data">
+        <b-list-group horizontal>
+          <b-list-group-item class="d-flex align-items-center light-color-text">
+            <h3>{{ data.item.first_name }} {{ data.item.last_name }}</h3>
+          </b-list-group-item>
+        </b-list-group>
+      </template>
+
+      <template #cell(status)="data">
+        {{ Status[data.item.status] }}
+      </template>
+
+      <template #cell(selected)="data">
+        <div v-if="data.rowSelected && data.item.status != -2">
+          <span aria-hidden="true">
+            <b-icon
+              font-scale="2"
+              variant="secondary"
+              icon="chevron-compact-up"
+            />
+          </span>
+        </div>
+        <div v-else-if="data.item.status != -2">
+          <span aria-hidden="true">
+            <b-icon
+              font-scale="2"
+              variant="secondary"
+              icon="chevron-compact-down"
+            />
+          </span>
+        </div>
+      </template>
+
+      <template #row-details="data">
+        <div v-if="data.item.status != -2" class="mb-2">
+          <h3> Profil de {{ data.item.first_name }} {{ data.item.last_name }} </h3>
+          <p> <b> E-mail: </b>  <span :id="'email-' + data.item.id"> unjour@yaura.com </span> </p>  <br>
+          <span> <b> Date de naissance: </b> {{ data.item.dob }} </span> <br />
+          <span> <b> Adresse: </b> {{ users[data.index].has_complete_address }} </span> <br />
+
+          <b-row>
+            <b-col>
+              <b> Changez le status </b> <br />
+              <b-select
+                :id="'select-' + data.item.user"
+                left
+                class="form-select"
+              >
+                <option :selected="data.item.status == -2" value="-2">Rejeté</option>
+                <option :selected="data.item.status == -1" value="-1">
+                  Abandonné
+                </option>
+                <option :selected="data.item.status == 0" value="0">Inscrit</option>
+                <option :selected="data.item.status == 1" value="1">
+                  Non sélectionné
+                </option>
+                <option :selected="data.item.status == 2" value="2">
+                  Sélectionné
+                </option>
+                <option :selected="data.item.status == 3" value="3">Accepté</option>
+                <option :selected="data.item.status == 4" value="4">Confirmé</option>
+              </b-select>
+            </b-col>
+          </b-row>
+        </div>
+      </template>
+    </b-table>
 
     <b-button variant="primary" @click="update"> Sauvegarder </b-button>
+    <b-button variant="primary" @click="dlInfo"> Téléchargement </b-button>
   </div>
 </template>
 <script>
-import Vue from 'vue';
-import { applicationsAPI } from '../../services/applications.api';
-import AppCardAdmin from './ApplicationCardAdmin.vue';
+import Vue from "vue";
+import { applicationsAPI } from "../../services/applications.api";
+import { usersAPI } from "../../services/users.api";
+
+import AppCardAdmin from "./ApplicationCardAdmin.vue";
 
 export default Vue.extend({
-  name: 'ApplicationListAdmin',
+  name: "ApplicationListAdmin",
   components: {
-    AppCardAdmin
+    AppCardAdmin,
   },
-  props: ['event_id'],
-  data () {
+  props: ["event_id"],
+  data() {
     return {
+      fields: ["name", {key: "status", sortable: true}, "Informations"],
       applications: [],
+      users: [],
       Status: {
         "-2": "Rejeté",
         "-1": "Abandonné",
-        "0": "Inscrit",
-        "1": "Non sélectionné",
-        "2": "Sélectionné",
-        "3": "Accepté",
-        "4": "Confirmé",
+        0: "Inscrit",
+        1: "Non sélectionné",
+        2: "Sélectionné",
+        3: "Accepté",
+        4: "Confirmé",
       }
     }
   },
   created() {
     applicationsAPI.applicationsList(null, this.event_id).then((res) => {
       this.applications = res;
-    })
+      this.applications.forEach((app) => {
+        usersAPI.usersMeRead(app.user).then((res) => {
+          this.users.push(res);
+        });
+      });
+      console.log(this.users);
+    });
   },
   methods: {
     async update_api(app, status) {
-      const form = {"status": status};
-      const req = await this.$axios.patch("/rest/v1/applications/" + app.id + "/update_status/", form);
+      const form = { status };
+      const req = await this.$axios.patch(
+        "/rest/v1/applications/" + app.id + "/update_status/",
+        form
+      );
       return req;
     },
     update() {
       this.applications.forEach((app) => {
-        const new_status = document.getElementById('select-' + app.user).value;
+        const new_status = document.getElementById("select-" + app.user).value;
         this.update_api(app, new_status);
         app.status = new_status;
-      });
+      })
     },
+    dlInfo() {
+      this.applications.forEach((app) => {
+        console.log(document.getElementById(app.user));
+      })
+    }
   }
 });
 </script>
