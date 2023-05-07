461c920af358c64d18f8ec31260946f5aedc776b
paul Dufour <paul.dufour@epita.fr>
2023-05-08 00:46:59 +0700

    front: add first draft account info page
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>

--- backend/gccsite/urls.py
+++ backend/gccsite/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("events.urls")),
     path("", include("pages.urls")),
+    path("", include("users.urls")),
 ]
-
-handler404 = "pages.views.error_404_view"
