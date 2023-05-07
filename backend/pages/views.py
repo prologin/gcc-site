f08bada1227a67b98e83e636cd5abb3a6fff1c00
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-05-07 21:55:27 +0200

    legal-notices: migrate legal notices page

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
+from gccsite.context_processors import my_context_processor
 
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
 
     def get_context_data(self, *args, **kwargs):
         return {}
+
+class LegalNoticesView(TemplateView):
+    template_name = 'pages/legal_notices.html'
+
+    def get_context_data(self, *args, **kwargs):
+        return my_context_processor(None)
