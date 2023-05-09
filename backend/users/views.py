6374d49efb97fe6943dc2fbf3fe261c7b52efaea
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-05-09 21:31:27 +0200

    django-templates(account-info): Add PersonalInfoForm

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,10 @@
 from typing import Any, Dict
 from django.views.generic import TemplateView
 from .models import User
-from .forms import ExampleForm
+from .forms import PersonalInfoForm
 
 class AccountInformationsView(TemplateView):
   template_name = 'users/AccountInformationsView.html'
 
   def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-    return {'form' : ExampleForm()}
+    return {'form' : PersonalInfoForm()}
