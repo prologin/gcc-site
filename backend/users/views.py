bda3fa3a00b1e9c3f90102c32d5d3c29ed5fec00
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-05-10 01:53:32 +0200

    django-templates(account-info): Add EmailForm

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,13 @@
 from typing import Any, Dict
 from django.views.generic import TemplateView
 from .models import User
-from .forms import PersonalInfoForm
+from .forms import PersonalInfoForm, EmailForm
 
 class AccountInformationsView(TemplateView):
   template_name = 'users/AccountInformationsView.html'
 
   def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-    return {'form' : PersonalInfoForm()}
+    return {
+      'personal_info_form' : PersonalInfoForm(),
+      'email_form' : EmailForm()
+      }
