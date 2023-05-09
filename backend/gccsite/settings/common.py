5e527d442e1608fac458d63c5279acb09a9a98a1
Paul Dufour <paul.paul.dufour@gmail.com>
2023-05-09 08:02:59 +0000

    front-page: fix minor bug and rm useless files
    Squashed commit of the following:
    
    * django-template(header): fix some icon issue
    
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>
    
    * django-templates(icons): remove fulfilled TODO
    
    * django-template(icons): Get rid of Bootstrap icons
    
    * django-templates(icons): Use FA icons for social networks
    
    * footer: SocialNetwork back to gccsite
    
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>
    
    * front: fix some path
    
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>
    
    * front: update file directory
    
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>
    
    * front-page: fix minor bug and rm useless files
    
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>

--- backend/gccsite/settings/common.py
+++ backend/gccsite/settings/common.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_bootstrap_icons",
     "events",
     "users",
     "pages",
