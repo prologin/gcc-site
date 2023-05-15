73f667c399465b98598b2028d3a92b76a691130d
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-05-15 23:09:32 +0200

    django-template(form): Prefill form with current info

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -7,9 +8,9 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     email = models.EmailField(
+        verbose_name=_("Adresse email"),
         null=False,
         blank=False,
-        verbose_name="Adresse email",
         unique=True,
     )
 
