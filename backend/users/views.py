83f5f418b23a34f1f7e400f335395058308d2bab
paul Dufour <paul.dufour@epita.fr>
2023-05-16 11:39:19 +0700

    django-template(form): password form
    Signed-off-by: paul Dufour <paul.dufour@epita.fr>

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,6 +6,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 
+# password check
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import update_session_auth_hash
+
 from .forms import (
     EmailForm,
     NotificationsUpdateForm,
@@ -17,6 +21,7 @@ from .models import User
 # Extra message tags
 TAG_PERSONAL_INFO = "personal_info"
 TAG_EMAIL = "email"
+TAG_PWD = "pwd"
 
 
 class AccountInformationsView(LoginRequiredMixin, TemplateView):
@@ -26,8 +31,8 @@ class AccountInformationsView(LoginRequiredMixin, TemplateView):
         if request.method == "POST":
             personal_info_form = PersonalInfoForm(request.POST)
             email_form = EmailForm(request.POST)
-            password_update_form = EmailForm(request.POST)
-            notifs_update_form = EmailForm(request.POST)
+            password_update_form = PasswordUpdateForm(request.POST)
+            notifs_update_form = NotificationsUpdateForm(request.POST)
 
             if personal_info_form.is_valid():
                 # user cannot be None because the page requires login
@@ -81,7 +86,43 @@ class AccountInformationsView(LoginRequiredMixin, TemplateView):
                 )
 
             elif password_update_form.is_valid():
-                return HttpResponse("Password update form valid")
+                # user cannot be None because the page requires login
+                user = User.objects.get(id=request.user.id)
+                current_pwd = request.POST["current_pwd"]
+
+                # check if currect_pwd == pwd
+                if check_password(current_pwd, user.password):
+                    new_pwd = request.POST["new_pwd"]
+                    
+                    # check if the 2 new passord are the same
+                    if new_pwd == request.POST["new_pwd_ack"]:
+                        user.set_password(new_pwd)
+                        user.save()
+                        update_session_auth_hash(request, user)
+                        # Send a message to display
+                        messages.success(
+                            request,
+                            "Votre mot de passe à été changé !",
+                            extra_tags=TAG_PWD,
+                        )
+                    else :
+                        # Send a Warning to display
+                        messages.warning(
+                            request,
+                            "Les deux mot de passes ne correspondent pas !",
+                            extra_tags=TAG_PWD,
+                        )
+                else :
+                    # Send a Warning to display
+                    messages.warning(
+                        request,
+                        "Votre mot de passe n'est pas bon !",
+                        extra_tags=TAG_PWD,
+                    )
+
+                return HttpResponseRedirect(
+                    reverse("users:account_information")
+                )
 
             elif notifs_update_form.is_valid():
                 return HttpResponse("Notifs update form valid")
