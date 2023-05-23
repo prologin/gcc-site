fff954b3cdb35c80c66df99ae361ef2dfba3c0b5
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-05-24 00:26:35 +0200

    users(login): Remove Logout view as django default works fine

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -11,5 +12,5 @@ urlpatterns = [
         name="account_information",
     ),
     path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.LogoutView.as_view(), name="logout"),
+    path("logout", LogoutView.as_view(), name="logout"),
 ]
