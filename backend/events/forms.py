debbd9275564697ed8109b223fc6210064024a79
Julien Bestard <julien.bestard@epita.fr>
2023-06-02 22:37:07 +0200

    continue application form

--- backend/events/forms.py
+++ backend/events/forms.py
@@ -10,11 +10,12 @@ from crispy_forms.layout import (
     Submit,
 )
 from django import forms
-from django.core.validators import validate_email
+from django.core.validators import validate_email, RegexValidator, MaxLengthValidator
 from django.utils.translation import gettext_lazy as _
 
 
 class EventSignupForm(forms.Form):
+    ### Info sur la participante
     first_name = forms.CharField(
         label="Prénom",
         max_length=256,
@@ -32,7 +33,7 @@ class EventSignupForm(forms.Form):
         widget=forms.widgets.DateInput(attrs={'type': 'date'}),
         required=True
     )
-    
+
     address = forms.CharField(
         label=("Addresse: Ville, zipcode, pays"),
         required=True
@@ -45,7 +46,14 @@ class EventSignupForm(forms.Form):
         validators=[validate_email],
     )
 
-    phone = forms.RegexField(regex="^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$")
+    phone = forms.DecimalField(
+        label="Numéro de téléphone",
+        validators=[RegexValidator(
+            regex="^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$", 
+            message="Not a phone number", 
+            code="invalid_phone")],
+        required=True,
+    )
 
     authorization = forms.BooleanField(
         label=_(
@@ -59,26 +67,76 @@ class EventSignupForm(forms.Form):
         required=True,
     )
 
+    ### Responsable légal
+
+
+
+    ### Info supplémentaires participantes
+
+    diet = forms.CharField(
+        label="As-tu un régime alimentaire particulier ?",
+        max_length=256,
+        required=True
+    )
+
+    allergies = forms.CharField(
+        label="As-tu des allergies ?",
+        max_length=256,
+        required=True
+    )
+
+    tshirt = forms.CharField(
+        label="Quelle est ta taille de t-shirt ?",
+        help_text="Un t-shirt sera donné aux participantes pendant le stage",
+        max_length=256,
+        required=True
+    )
+
+    learn = forms.CharField(
+        label="Prénom",
+        max_length=256,
+        required=True
+    )
+
+    programing = forms.CharField(
+        label="Prénom",
+        max_length=256,
+        required=True
+    )
+
+    studies = forms.CharField(
+        label="Prénom",
+        max_length=256,
+        required=True
+    )
+
+    association = forms.CharField(
+        label="Prénom",
+        max_length=256,
+        required=True
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = "application_form"
         self.helper.form_method = "post"
-
         self.helper.layout = Layout(
-            Div(
-                Button(
-                    name="self_inscription",
-                    value="M'inscrire",
-                    css_class = "next-click btn primary-button",
-                ),
-                Button(
-                    name="child_inscription",
-                    value="Inscrire ma fille",
-                    css_class = "next-click btn primary-button",
-                ),
-                css_class = "tab active"
-            ),
+            # Div(
+            #    Button(
+            #        id="self-inscription",
+            #        name="self-inscription",
+            #        value="M'inscrire",
+            #        css_class = "next-click btn primary-button",
+            #    ),
+            #    Button(
+            #        id="child-inscription",
+            #         name="child-inscription",
+            #         value="Inscrire ma fille",
+            #         css_class = "next-click btn primary-button",
+            #     ),
+            #     css_class = "tab active"
+            # ),
             Div(
                 Field("first_name"),
                 Field("last_name"),
@@ -89,17 +147,35 @@ class EventSignupForm(forms.Form):
                 Field("authorization"),
                 Div(
                     Button(
+                        name="next",
+                        value="Suivant",
+                        css_class="btn primary-button"
+                    ),
+                    css_class="mt-4 p-0"
+                ),
+                css_class="tab active"
+            ),
+            Div(
+                Field("tshirt"),
+                Field("allergies"),
+                Field("diet"),
+                Field("learn"),
+                Field("programing"),
+                Field("studies"),
+                Field("association"),
+                Div(
+                    Button(
                         name="back",
                         value="Retour",
-                        css_class="back-click btn primary-button",
+                        css_class="btn primary-button"
                     ),
                     Button(
                         name="next",
                         value="Suivant",
-                        css_class="next-click btn primary-button",
+                        css_class="btn primary-button"
                     ),
-                    css_class="mt-4 p-0",
+                    css_class="mt-4 p-0"
                 ),
-                css_class = "tab"
+                css_class="tab"
             )
         )
