dc5de1f995a1f4561ce993dafd6a6109aa2ab8e9
Dorian 'renji' Péron <dorianperon.i@gmail.com>
2023-06-18 17:23:20 +0200

    registration(privacy): Add disclaimer text on registration form's template

--- website/pages/urls.py
+++ website/pages/urls.py
@@ -5,8 +5,24 @@ from . import views
 app_name = "pages"
 
 urlpatterns = [
-    path("about/", views.AboutView.as_view()),
-    path("legal-notices/", views.LegalNoticesView.as_view()),
-    path("privacy/", views.PrivacyView.as_view()),
-    path("faq/", views.FAQView.as_view()),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path(
+        "legal-notices/",
+        views.LegalNoticesView.as_view(),
+        name="legal_notices",
+    ),
+    path("privacy/", views.PrivacyView.as_view(), name="privacy"),
+    # TODO: Create appropriate pages
+    path(
+        "privacy-inscription/",
+        views.PrivacyView.as_view(),
+        name="privacy_inscription",
+    ),
+    path("privacy-stage/", views.PrivacyView.as_view(), name="privacy_stage"),
+    path(
+        "privacy-newsletter/",
+        views.PrivacyView.as_view(),
+        name="privacy_newsletter",
+    ),
+    path("faq/", views.FAQView.as_view(), name="FAQ"),
 ]
