694a7c40371be4f8937312ea23e1805dac0858a1
Dorian Péron <dorianperon.i@gmail.com>
2023-09-08 19:18:07 +0200

    front(application-form): Move logic from form_flow.js to application_form.js

--- website/static/js/application_form.js
+++ website/static/js/application_form.js
@@ -1,6 +1,19 @@
+const PHONE_REGEX = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
 const INVALID_CLASS = "is-invalid";
 const TAB_ACTIVE_CLASS = "active";
 
+/// Take a form HTML element and a positive integer as arguments.
+/// Add display: none to all the disabled tabs.
+function formDisplayNthTab(form, n) {
+    form.querySelectorAll(".tab")
+        .forEach((tab, index) => {
+            if (index === n) {
+                tab.classList.add(TAB_ACTIVE_CLASS);
+            } else {
+                tab.classList.remove(TAB_ACTIVE_CLASS);
+            }
+        });
+}
 
 /// Take the form HTML element in argument.
 /// - Removes all user added data and reset all placeholders to defaults
@@ -14,7 +27,7 @@ function clearApplicationForm(form) {
             // If not a user-editable input, return
             if (type === "hidden"     // CRSF token etc...
                 || type === "button"  // prev/next/submit btns
-                ) {
+            ) {
                 return;
             }
 
@@ -37,20 +50,49 @@ function clearApplicationForm(form) {
         });
 
     // Reset application form tab.
-    form.querySelectorAll(".tab")
-        .forEach((tab, index) => {
-            if (index === 0) {
-                tab.classList.add(TAB_ACTIVE_CLASS);
-            } else {
-                tab.classList.remove(TAB_ACTIVE_CLASS);
+    formDisplayNthTab(form, 0);
+}
+
+/// Take the form HTML element as argument.
+/// @return true if the current form tab is correctly filled.
+function validateApplicationForm(form) {
+    let valid = true;
+    let active_tab = form.querySelector(".tab.active");
+    let fields = active_tab.querySelectorAll("input, select");
+    fields.forEach(function (val) {
+        val.classList.remove(INVALID_CLS);
+
+        if (val.hasAttribute("required")) {
+            if (val.type === "checkbox" || val.type === "radio") {
+                if (!val.checked) {
+                    valid = false;
+                    val.classList.add(INVALID_CLS);
+                }
+            } else if (val.tagName === "select") {
+                // Handle select elements (dropdowns)
+                if (val.value === "") {
+                    valid = false;
+                    val.classList.add(INVALID_CLS);
+                }
+            } else if (val.value.length == 0) {
+                valid = false;
+                val.classList.add(INVALID_CLS);
+            } else if (val.type === "tel") {
+                // Handle phone validation
+                if (!PHONE_REGEX.test(val.value)) {
+                    valid = false;
+                    val.classList.add(INVALID_CLS);
+                }
             }
-        });
+        }
+    });
+    return validate;
 }
 
 /// Take the modal id in argument
 /// Add an event listener to the modal to reset the form upon opening.
 function setupApplicationFormModal(modal_id) {
-    // The application form modal should only be inserted once.
+
     const application_form_modal = document.getElementById(modal_id);
 
     // If the user is not authenticated, form will be null.
@@ -60,6 +102,9 @@ function setupApplicationFormModal(modal_id) {
         return;
     }
 
+    // Add event effects on modal opening.
+    // - Update event_id
+    // - Clear form, go to 1st tab
     application_form_modal.addEventListener("show.bs.modal", js_event => {
 
         // When the modal appears, find the id of the event related
@@ -78,6 +123,31 @@ function setupApplicationFormModal(modal_id) {
         }
         event_it_input.setAttribute("value", event_id);
     });
+
+    // Add event listeners for prev/next buttons
+    const next_buttons = form.querySelectorAll("#button-id-next");
+    const back_buttons = form.querySelectorAll("#button-id-back");
+    let tab_index = 0;
+
+    next_buttons.forEach(function (btn) {
+        btn.addEventListener("click",
+            function () {
+                // Do not go to next page if there is invalid data
+                if (!validateApplicationForm(form)) {
+                    return false;
+                }
+                formDisplayNthTab(++tab_index);
+            })
+    });
+    back_buttons.forEach(function (btn) {
+        btn.addEventListener("click",
+            function () {
+                formDisplayNthTab(--tab_index);
+            })
+    });
+
+    // Ensure the first tab is opened at page loading
+    formDisplayNthTab(tab_index);
 }
 
 setupApplicationFormModal("application-form-modal");
