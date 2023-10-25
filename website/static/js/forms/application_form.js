22ff3cbe131a9e8ba9ff93571adb9261123009a1
Dorian Péron <dorianperon.i@gmail.com>
2023-10-25 22:41:42 +0000

    [FEAT]: Press 'enter' to go to the next form tab
    Squashed commit of the following:
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * feat(form.js): Remove old unused form code
    
    * feat(form.js): Refactor the register form JS with multitab class
    
    * feat(form.js): Refactor the multitab form handling into a class, Add 'enter' listener to go to next page

--- website/static/js/forms/application_form.js
+++ website/static/js/forms/application_form.js
@@ -1,42 +1,4 @@
-import { INVALID_CLASS, formDisplayNthTab, validateForm } from "./form.js"
-
-/// Take the form HTML element in argument.
-/// - Removes all user added data and reset all placeholders to defaults
-/// - Set the view to the 1st tab.
-function clearApplicationForm(form) {
-    // Select all user inputs
-    form.querySelectorAll("input")
-        .forEach(input => {
-            const type = input.getAttribute("type");
-
-            // If not a user-editable input, return
-            if (type === "hidden"     // CRSF token etc...
-                || type === "button"  // prev/next/submit btns
-            ) {
-                return;
-            }
-
-            // Remove the red warning if present
-            input.classList.remove(INVALID_CLASS);
-            // Reset the value to the default
-            if (type === "checkbox") {
-                input.checked = false;
-            } else {
-                input.value = input.getAttribute("value");
-            }
-        });
-
-    // Reset selectors to default choice
-    form.querySelectorAll("select")
-        .forEach(select => {
-            // Remove the red warning if present.
-            select.classList.remove(INVALID_CLASS);
-            select.selectedIndex = 0;
-        });
-
-    // Reset application form tab.
-    formDisplayNthTab(form, 0);
-}
+import { MultiTabForm } from "./form.js"
 
 /// Take the modal id in argument
 /// Add an event listener to the modal to reset the form upon opening.
@@ -47,13 +9,15 @@ function setupApplicationFormModal(modal_id) {
     // If the user is not authenticated, form will be null.
     // We will have nothing to do
     const form = application_form_modal.querySelector("form");
-    if (form == null) {
+    if (form === null) {
         return;
     }
 
+    let multitabform = new MultiTabForm(form);
+
     // Add event effects on modal opening.
     // - Update event_id
-    // - Clear form, go to 1st tab
+    // - Reset form if id is changed
     application_form_modal.addEventListener("show.bs.modal", js_event => {
 
         // When the modal appears, find the id of the event related
@@ -68,35 +32,10 @@ function setupApplicationFormModal(modal_id) {
         const previous_id = event_it_input.getAttribute("value");
 
         if (previous_id != null && previous_id != event_id) {
-            clearApplicationForm(form);
+            multitabform.reset();
         }
         event_it_input.setAttribute("value", event_id);
     });
-
-    // Add event listeners for prev/next buttons
-    const next_buttons = form.querySelectorAll("#button-id-next");
-    const back_buttons = form.querySelectorAll("#button-id-back");
-    let tab_index = 0;
-
-    next_buttons.forEach(function (btn) {
-        btn.addEventListener("click",
-            function () {
-                // Do not go to next page if there is invalid data
-                if (!validateForm(form)) {
-                    return false;
-                }
-                formDisplayNthTab(form, ++tab_index);
-            })
-    });
-    back_buttons.forEach(function (btn) {
-        btn.addEventListener("click",
-            function () {
-                formDisplayNthTab(form, --tab_index);
-            })
-    });
-
-    // Ensure the first tab is opened at page loading
-    formDisplayNthTab(form, tab_index);
 }
 
 setupApplicationFormModal("application-form-modal");
--- website/static/js/forms/form.js
+++ website/static/js/forms/form.js
@@ -6,63 +6,158 @@ const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"
 const INVALID_CLASS = "is-invalid";
 const TAB_ACTIVE_CLASS = "active";
 
-/// Take a form HTML element and a positive integer as arguments.
-/// Add display: none to all the disabled tabs.
-function formDisplayNthTab(form, n) {
-    form.querySelectorAll(".tab")
-        .forEach((tab, index) => {
-            if (index === n) {
-                tab.classList.add(TAB_ACTIVE_CLASS);
-            } else {
-                tab.classList.remove(TAB_ACTIVE_CLASS);
+class MultiTabForm {
+    form;
+    next_buttons;
+    back_buttons;
+    tabs;
+    tab_index = 0;
+    last_tab_index;
+
+    constructor(form) {
+        this.form = form;
+        this.next_buttons = form.querySelectorAll("#button-id-next");
+        this.back_buttons = form.querySelectorAll("#button-id-back");
+
+        this.tabs = form.querySelectorAll(".tab");
+        this.last_tab_index = this.tabs.length - 1;
+
+        // Set event listeners
+        this.next_buttons.forEach((btn) => {
+            btn.addEventListener("click",
+                () => {
+                    console.log(this);
+                    return this.gotoNextTab();
+                }
+            )
+        });
+        this.back_buttons.forEach((btn) => {
+            btn.addEventListener("click",
+                () => {
+                    return this.gotoPrevTab();
+                }
+            )
+        });
+        this.form.addEventListener("keydown",
+            (event) => {
+                if (event.key !== "Enter") {
+                    return false;
+                }
+                if (this.tab_index === this.last_tab_index) {
+                    // Do not do anything and let the submit button handler submit
+                    return false;
+                } else {
+                    return this.gotoNextTab();
+                }
+            }
+        );
+
+        // Make sure the first tab is showed
+        this.setActiveTab();
+    }
+
+    reset() {
+        this.form.querySelectorAll("input")
+            .forEach(input => {
+                const type = input.getAttribute("type");
+
+                // If not a user-editable input, return
+                if (type === "hidden"     // CRSF token etc...
+                    || type === "button"  // prev/next/submit btns
+                ) {
+                    return;
+                }
+
+                // Remove the red warning if present
+                input.classList.remove(INVALID_CLASS);
+
+                // Reset the value to the default
+                if (type === "checkbox") {
+                    input.checked = false;
+                } else {
+                    input.value = input.getAttribute("value");
+                }
+            });
+
+        // Reset selectors to default choice
+        this.form.querySelectorAll("select")
+            .forEach((select) => {
+                // Remove the red warning if present.
+                select.classList.remove(INVALID_CLASS);
+                select.selectedIndex = 0;
+            });
+    }
+
+    validateCurrentTab() {
+        let valid = true;
+        let fields = this.tabs[this.tab_index].querySelectorAll("input, select");
+
+        fields.forEach((field) => {
+            field.classList.remove(INVALID_CLASS);
+
+            if (field.hasAttribute("required")) {
+                if (field.type === "checkbox" || field.type === "radio") {
+                    if (!field.checked) {
+                        valid = false;
+                        field.classList.add(INVALID_CLASS);
+                    }
+                } else if (field.tagName === "select") {
+                    // Handle select elements (dropdowns)
+                    if (field.value === "") {
+                        valid = false;
+                        field.classList.add(INVALID_CLASS);
+                    }
+                } else if (field.value.length == 0) {
+                    valid = false;
+                    field.classList.add(INVALID_CLASS);
+                } else if (field.type === "tel") {
+                    // Handle phone validation
+                    if (!PHONE_REGEX.test(field.value)) {
+                        valid = false;
+                        field.classList.add(INVALID_CLASS);
+                    }
+                } else if (field.type === "email") {
+                    if (!EMAIL_REGEX.test(field.value)) {
+                        valid = false;
+                        field.classList.add(INVALID_CLASS);
+                    }
+                }
             }
         });
-}
+        return valid;
+    }
 
-/// Take the form HTML element as argument.
-/// @return true if the current form tab is correctly filled.
-function validateForm(form) {
-    let valid = true;
-    let active_tab = form.querySelector(".tab.active");
-    let fields = active_tab.querySelectorAll("input, select");
-    fields.forEach(function (val) {
-        val.classList.remove(INVALID_CLASS);
+    setActiveTab() {
+        this.tabs
+            .forEach((tab, index) => {
+                if (index === this.tab_index) {
+                    tab.classList.add(TAB_ACTIVE_CLASS);
+                } else {
+                    tab.classList.remove(TAB_ACTIVE_CLASS);
+                }
+            });
+    }
 
-        if (val.hasAttribute("required")) {
-            if (val.type === "checkbox" || val.type === "radio") {
-                if (!val.checked) {
-                    valid = false;
-                    val.classList.add(INVALID_CLASS);
-                }
-            } else if (val.tagName === "select") {
-                // Handle select elements (dropdowns)
-                if (val.value === "") {
-                    valid = false;
-                    val.classList.add(INVALID_CLASS);
-                }
-            } else if (val.value.length == 0) {
-                valid = false;
-                val.classList.add(INVALID_CLASS);
-            } else if (val.type === "tel") {
-                // Handle phone validation
-                if (!PHONE_REGEX.test(val.value)) {
-                    valid = false;
-                    val.classList.add(INVALID_CLASS);
-                }
-            } else if (val.type === "email") {
-                if (!EMAIL_REGEX.test(val.value)) {
-                    valid = false;
-                    val.classList.add(INVALID_CLASS);
-                }
-            }
+    gotoNextTab() {
+        if (this.tab_index === this.last_tab_index) {
+            return false;
         }
-    });
-    return valid;
+        if (!this.validateCurrentTab()) {
+            return false;
+        }
+        this.tab_index++;
+        this.setActiveTab();
+    }
+
+    gotoPrevTab() {
+        if (this.tab_index === 0) {
+            return false;
+        }
+        this.tab_index--;
+        this.setActiveTab();
+    }
 }
 
 export {
-    PHONE_REGEX,
-    INVALID_CLASS,
-    formDisplayNthTab,
-    validateForm
+    MultiTabForm,
 };
