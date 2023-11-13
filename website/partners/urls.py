73cf58efbe0112d69a90d2cdd5d63202da1a2dd8
Paul Dufour <paul.dufour@prologin.org>
2023-11-13 23:44:30 +0100

    add: changes
    Signed-off-by: Paul Dufour <paul.dufour@prologin.org>

--- website/partners/urls.py
+++ website/partners/urls.py
@@ -7,5 +7,5 @@ from django.views.decorators.cache import cache_page
 app_name = "partners"
 
 urlpatterns = [
-    path("partners/", cache_page(60*60)(views.PartnersView.as_view()), name="partners"),
+    path("partners/", views.PartnersView.as_view(), name="partners"),
 ]
