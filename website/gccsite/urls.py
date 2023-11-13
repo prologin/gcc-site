820cd432d6d3724c443bc9e7a600ea173b29b211
Paul Dufour <paul.dufour@prologin.org>
2023-11-13 22:04:23 +0100

    fix: clem est giga nul
    Signed-off-by: Paul Dufour <paul.dufour@prologin.org>

--- website/gccsite/urls.py
+++ website/gccsite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,7 +11,7 @@ urlpatterns = [
     path("", include("users.urls")),
     path("", include("partners.urls")),
     path("rest/auth/oidc/", include("mozilla_django_oidc.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
