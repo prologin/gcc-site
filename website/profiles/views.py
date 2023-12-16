be780e014b82770373c3d133bc6cfd9b399438f3
Dorian Péron <dorianperon.i@gmail.com>
2023-12-16 16:52:58 +0100

    feat(profiles): Simplify the CreateView, MOVE messages rendering from home.html to base.html

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -16,53 +16,45 @@ from profiles.models import Profile
 class CreateProfileView(LoginRequiredMixin, CreateView):
     template_name = "profiles/create_profiles.html"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("profiles:profile_list")  # TODO: Update this
+    success_url = reverse_lazy("profiles:profiles_list")  # TODO: Update this
 
     def post(self, request, *args, **kwargs):
-        redirect_url = request.META.get("HTTP_REFERER", None)
-        if not redirect_url:
-            redirect_url = reverse("profiles:create_profile")
+        self.object = None
 
-        if "submit-application" in request.POST:
-            form = ProfileCreationForm(request.POST)
+        form = self.get_form()
 
-            if not form.is_valid():
-                messages.warning(
-                    request,
-                    str(form.errors),
-                )
-                return HttpResponseRedirect(redirect_url)
-            else:
-                user = get_user_model().objects.get(id=request.user.id)
+        if not form.is_valid():
+            messages.warning(
+                request,
+                str(form.errors),
+            )
+            return self.form_invalid(form)
+        else:
+            address = form.clean_address()
 
-                address = form.clean_address()
+            address_resp = form.clean_address_resp()
 
-                address_resp = form.clean_address_resp()
+            school = form.clean_school_info()
 
-                school = form.clean_school_info()
+            profile = Profile.objects.create(
+                user=self.request.user,
+                first_name=form.cleaned_data["first_name"],
+                last_name=form.cleaned_data["last_name"],
+                birth_date=form.cleaned_data["birth_date"],
+                email=form.cleaned_data["email"],
+                phone=form.cleaned_data["phone"],
+                address=address,
+                first_name_resp=form.cleaned_data["first_name_resp"],
+                last_name_resp=form.cleaned_data["last_name_resp"],
+                email_resp=form.cleaned_data["email_resp"],
+                phone_resp=form.cleaned_data["phone"],
+                address_resp=address_resp,
+                school=school,
+            )
 
-                profile = Profile.objects.create(
-                    user=user,
-                    first_name=form.cleaned_data["first_name"],
-                    last_name=form.cleaned_data["last_name"],
-                    birth_date=form.cleaned_data["birth_date"],
-                    email=form.cleaned_data["email"],
-                    phone=form.cleaned_data["phone"],
-                    address=address,
-                    first_name_resp=form.cleaned_data["first_name_resp"],
-                    last_name_resp=form.cleaned_data["last_name_resp"],
-                    email_resp=form.cleaned_data["email_resp"],
-                    phone_resp=form.cleaned_data["phone"],
-                    address_resp=address_resp,
-                    school=school,
-                )
+            messages.success(request, "Votre profil a été enregistré !")
 
-                messages.success(request, "Votre profil a été enregistrée!")
-
-                return HttpResponseRedirect(redirect_url)
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        raise Http404()
+            return HttpResponseRedirect(self.success_url)
 
 
 class ProfileListView(LoginRequiredMixin, ListView):
