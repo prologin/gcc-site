e66e2cd72d50349943f5c34a26f553d7cc6842f1
Paul Dufour <paul.dufour@prologin.org>
2023-12-16 11:48:59 +0100

    fix: url path
    Signed-off-by: Paul Dufour <paul.dufour@prologin.org>

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -13,17 +13,5 @@ from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
 class CreateProfileView(CreateView):
-    template_name = "profiles/profile_create.html"
+    template_name = "profiles/create_profiles.html"
     form_class = ProfileCreationForm
-
-    @method_decorator(csrf_protect)
-    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        values = self.form_class(request.POST)
-        if not values.is_valid():
-            return HttpResponse(values.errors.as_json(), status=400)
-
-        profile = values.save(commit=False)
-        profile.owner = request.user;
-        profile.save()
-
-        return HttpResponse(status=201)
