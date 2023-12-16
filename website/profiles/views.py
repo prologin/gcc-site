91ba0326fa62a09c55fefdbefee68ac3b783fa93
Dorian Péron <dorianperon.i@gmail.com>
2023-12-16 12:23:57 +0100

    fix: URL resolve

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.views.generic.edit import CreateView
 
 from profiles.forms import ProfileCreationForm
 
-from django.views.generic.edit import CreateView
-from django.utils.decorators import method_decorator
-
-from django.http import HttpRequest, HttpResponse
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-
-
 # Create your views here.
+
 
 class CreateProfileView(CreateView):
     template_name = "profiles/create_profiles.html"
     form_class = ProfileCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL  # TODO: Update this
