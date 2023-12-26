c7ab519d3475c7fefe5b8657e3d8bfee4c37cb84
Dorian Péron <dorianperon.i@gmail.com>
2023-12-27 00:04:22 +0100

    feat(documents): Add a 10MB file size limit to documents

--- website/events/validators.py
+++ website/events/validators.py
@@ -1,5 +1,6 @@
 import magic
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 def validate_is_pdf(file):
@@ -9,3 +10,13 @@ def validate_is_pdf(file):
 
     if file_mime_type not in valid_mime_types:
         raise ValidationError("Unsupported file type.")
+
+
+def validate_file_max_size(max_size_mb: int):
+    def validator(file):
+        if file.size > max_size_mb * (1 << 10):
+            raise ValidationError(
+                _("File size is too big (Max is %sMB)") % max_size_mb
+            )
+
+    return validator
