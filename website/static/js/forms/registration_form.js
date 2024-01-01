778164463c62a68db03ee196321e7aa3b8f1fa0a
Léo Portemont <leo1.portemont@epita.fr>
2024-01-01 14:29:10 +0100

    fix(users: registration): fix registration forms & views
    style: formatting

--- website/static/js/forms/registration_form.js
+++
@@ -1,8 +0,0 @@
-import { MultiTabForm } from "./form.js"
-
-/// Take the form HTML element as argument
-function setupRegistrationForm(form)  {
-    let multitabform = new MultiTabForm(form);
-}
-
-setupRegistrationForm(document.querySelector("form"));
